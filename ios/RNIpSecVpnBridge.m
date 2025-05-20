83067d399ee2452391d3b73ce9b52719e65625bb
Hasan Kürşat Küçüköztaş <hkucukoztas@gmail.com>
2025-05-21 00:23:00 +0200

    fix: change bridge

--- ios/RNIpSecVpnBridge.m
+++ ios/RNIpSecVpnBridge.m
@@ -11,8 +11,17 @@
 @interface RCT_EXTERN_MODULE(RNIpSecVpn, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(prepare:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(connect:(NSString *)address username:(NSString *)username password:(NSString *)password vpnType:(NSString *)vpnType mtu:(NSNumber *_Nonnull)mtu findEventsWithResolver:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(disconnect:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+RCT_EXTERN_METHOD(connect:(NSString *)address
+                  username:(NSString *)username
+                  password:(NSString *)password
+                  vpnType:(NSString *)vpnType
+                  enableKillSwitch:(BOOL)enableKillSwitch
+                  mtu:(NSNumber *_Nonnull)mtu
+                  findEventsWithResolver:(RCTPromiseResolveBlock)findEventsWithResolver
+                  rejecter:(RCTPromiseRejectBlock)rejecter)
+RCT_EXTERN_METHOD(disconnect:(BOOL)enableKillSwitch
+                  findEventsWithResolver:(RCTPromiseResolveBlock)findEventsWithResolver
+                  rejecter:(RCTPromiseRejectBlock)rejecter)
 RCT_EXTERN_METHOD(getCurrentState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
 RCT_EXTERN_METHOD(getCharonErrorState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
 
